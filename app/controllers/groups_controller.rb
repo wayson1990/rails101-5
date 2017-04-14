25b5ac14bc9b4840deba4727d6de4004e52df4f8
wayson <xwaysen@gmail.com>
2017-04-14 11:51:30 +0800

    check owner permission when access edit/update/delete

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 def index
   @groups = Group.all
 end
